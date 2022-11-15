cbab839d89420a2fa896b714ad2211bb4b85e1d1
Vidya  Anand <113001817+justvidya@users.noreply.github.com>
2022-11-15 17:40:52 +0000

    prob 3

---
+++ set02/problem03.c
@@ -0,0 +1,42 @@
+//Write a program to find the weight of the camel given height, length and stomach radius using four functions (Structures)
+#include <stdio.h>
+#include<math.h>
+
+struct camel
+{
+float rad,hei,len,wei;
+};
+typedef struct camel Camel;
+
+Camel input()
+{
+  Camel c;
+  printf("Enter the radius\n");
+  scanf("%f",&c.rad);
+  printf("Enter the height");
+  scanf("%f",&c.hei);
+  printf("Enter the lenght\n");
+  scanf("%f",&c.len);
+  return c;
+}
+
+void find_weight(Camel *c)
+{
+  int a,b,d;
+  a=pow(c->rad,3);
+  b=c->hei*c->len;
+  d=sqrt(b);
+  c->wei=3.14*a*d;
+}
+
+void output(Camel c)
+{ 
+  printf("The weight of camel is %f\n",c.wei);
+}
+int main()
+  {
+   Camel z;
+  z= input();
+  find_weight(&z);
+    output(z);
+  }
