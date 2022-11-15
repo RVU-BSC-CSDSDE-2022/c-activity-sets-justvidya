32a80ef9a2c5727caf0b9b338a1133df34736c41
Vidya  Anand <113001817+justvidya@users.noreply.github.com>
2022-11-15 16:54:47 +0000

    redid and corrected errors in problem 1

--- set02/problem01.c
+++ set02/problem01.c
@@ -1,47 +1,45 @@
+//write program to find the distance between two points
 #include<stdio.h>
 #include<math.h>
-
 struct _point
 {
-float x;
-float y;
+float x,y;
 };
 typedef struct _point Point;
 
 Point input()
 {
   Point a;
-  printf("Enter the co-ordinates of point\n");
+  printf("Enter the x co-ordinates of point\n");
   scanf("%f",&a.x);
+  printf("Enter the y co-ordinates of point\n");
   scanf("%f",&a.y);
   return a;
 }
-
-float dist(Point j, Point h)
+float dist(Point j,Point h)
 {
-  float res1, res2;
+  float res1,res2;
   float distance,p,q,r;
   res1=h.x-j.x;
-  res2=h.y-j.x;
+  res2=h.y-j.y;
   p=pow(res1,2);
-  q=pow(res2,2);
+  q=pow(res1,2);
   r=p+q;
   distance=sqrt(r);
   return distance;
 }
 
-void output(Point a, Point b,  float distance)
+void output(Point a, Point b, float distance)
 {
-  printf("Distance between (%f,%f) and(%f,%f) is %f",a.x, a.y, b.x, b.y, distance);
-  
+  printf("The distance between (%f,%f) and (%f,%f) is %f",a.x,a.y,b.x,b.y,distance);
 }
 
-int main(void)
+int main()
 {
-  Point e, f;
-  float g;
-  e=intput();
-  f=input();
-  g=dist(e,f);
-  output(e,f,g);
+  Point j,h;
+  float i;
+  j=input();
+  h=input();
+  i=dist(j,h);
+  output(j,h,i);
 }
