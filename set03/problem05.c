50a260e5b5e151cd6aad563adf019c8b63b68523
Vidya  Anand <113001817+justvidya@users.noreply.github.com>
2022-11-27 18:28:01 +0000

    did s3 p5

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,1 +1,39 @@
 //Write a program to find GCD (HCF) of two numbers
+#include <stdio.h>
+int input()
+{
+  int num;
+  scanf("%d", &num);
+  return num;
+}
+int find_gcd(int a, int b)
+{
+    int i, min, hcf=1;
+    /* Find minimum between two numbers */
+    min = (a<b) ? a : b;
+
+    for(i=1; i<=min; i++)
+    {
+        /* If i is factor of both number */
+        if(a%i==0 && b%i==0)
+        {
+            hcf = i;
+        }
+    }
+    return hcf;
+}
+
+
+void output(int a, int b, int gcd)
+{
+  printf("HCF of %d and %d = %d\n", a, b, gcd);
+}
+
+int main(void)
+{
+  int num1, num2, gcd;
+  num1=input();
+  num2=input();
+  gcd=find_gcd(num1,num2);
+  output(num1 ,num2 ,gcd);
+}
